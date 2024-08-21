4800e1f867361da25dabbf7a55a779c3b712c3f4
Tatiana3490 <tatiana.alcubilla.m@gmail.com>
2024-08-21 20:58:47 +0200

    Elimino código en details.html y lo pego en el detail.js. Y sigue funcionando

--- js/detail.js
+++ js/detail.js
@@ -1,33 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const params = new URLSearchParams(window.location.search);
-    const characterId = params.get('id'); 
-    const detailContainer = document.getElementById('personaje-detail');
+    const characterId = params.get('id');
+    const detailContainer = document.getElementById('character-detail');
 
-    if (characterId) {
-        fetch(`https://rickandmortyapi.com/api/character/${characterId}`)
-            .then(response => response.json())
-            .then(data => {
-                detailContainer.innerHTML = `
-                    <h2>${character.name}</h2>
-                        <img src="${character.image}" alt="${character.name}">
-                        <div class="character-info">
-                        <p><b>Id:</b> ${character.id}</p>
-                        <p><b>Estado:</b> ${character.status}</p>
-                        <p><b>Especie:</b> ${character.species}</p>
-                        <p><b>Género:</b> ${character.gender}</p>
-                        <p><b>Origen:</b> ${character.origin.name}</p>
-                        <p><b>Localización:</b> ${character.location.name}</p>   
-                                         
-                        </div>
-                        
-                `;
-         
-            })
-            .catch(error => {
-                detailContainer.innerHTML = `<p>Error al cargar los detalles del personaje.</p>`;
-                console.error('Error al obtener los detalles de los personajes:', error);
-            });
-    } else {
-        detailContainer.innerHTML = `<p>No se ha proporcionado ningún ID de personaje.</p>`;
+    if (!characterId) {
+        detailContainer.innerHTML = '<p>ID del personaje no proporcionado.</p>';
+        return;
     }
+
+    fetch(`https://rickandmortyapi.com/api/character/${characterId}`)
+        .then(response => response.json())
+        .then(character => {
+            detailContainer.innerHTML = `
+                <h2>${character.name}</h2>
+                <img src="${character.image}" alt="${character.name}">
+                <p><b>Id:</b> ${character.id}</p>
+                <p><b>Estado:</b> ${character.status}</p>
+                <p><b>Especie:</b> ${character.species}</p>
+                <p><b>Género:</b> ${character.gender}</p>
+                <p><b>Origen:</b> ${character.origin.name}</p>
+                <p><b>Localización:</b> ${character.location.name}</p>
+                <p><b>Creado:</b> ${character.created}</p>
+                                        
+            `;
+        })
+        .catch(error => {
+            detailContainer.innerHTML = '<p>Error al cargar los detalles del personaje. Inténtalo de nuevo más tarde.</p>';
+            console.error('Error:', error);
+        });
 });
